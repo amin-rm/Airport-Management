9810d6d628948fa1ec816dbe2a42d7e2a7abe281
amin-rm <ramdhaniahmedamine@gmail.com>
2024-10-04 23:47:34 +0100

    pushed solution

---
+++ AM.ApplicationCore/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace AM.ApplicationCore.Interfaces
+{ 
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        void Delete(Expression<Func<TEntity, bool>> where);
+        //Delete(x=>x.UserName.Equals(nom));
+       
+        TEntity GetById(params object[] id);
+        TEntity Get(Expression<Func<TEntity, bool>> where);
+        IEnumerable<TEntity> GetAll(); // GetMany()
+        IEnumerable<TEntity> GetMany(Expression<Func<TEntity, bool>> where = null);
+    }    
+}
---
+++ AM.ApplicationCore/Services/ServicePlane.cs
@@ -0,0 +1,22 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Services
+{
+    public class ServicePlane : Service<Plane>, IServicePlane
+    {
+        public ServicePlane(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
+
+        public IEnumerable<Passenger> GetPassengersByPlane(Plane p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
